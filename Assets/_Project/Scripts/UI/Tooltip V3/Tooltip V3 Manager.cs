25e88a0c91964b11ed5397c5c83b608a50ac6a43
Gaida <kaliguri@bk.ru>
2024-09-26 05:10:43 +0300

    Завершение работы над динамическим фоном для Tooltip

--- Assets/_Project/Scripts/UI/Tooltip V3/Tooltip V3 Manager.cs
+++ Assets/_Project/Scripts/UI/Tooltip V3/Tooltip V3 Manager.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using TMPro;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Localization.Components;
 
@@ -11,10 +12,13 @@ public class TooltipV3Manager : MonoBehaviour
     public LocalizeStringEvent Description;
     public LocalizeStringEvent NarrativeDescription;
 
-    [Title("All Dynamic Background")]
+    [Title("Dynamic Background")]
+    [Title("Dynamic Background Reference")]
     public RectTransform BackgroundRect;
-    public RectTransform ScaleObject;
-    public List<RectTransform> AllTextRectItems;
+    public RectTransform EndRectItem;
+    [Title("Dynamic Background Settings")]
+    public float ExtraWidthForBackground;
+    public float ExtraHeightForBackground;
 
     [Title("Tooltip Style")]
     public TooltipStyle Style;
@@ -31,58 +35,10 @@ public class TooltipV3Manager : MonoBehaviour
 
     void СalculationDynamicRectSizeLegacy()
     {
-        var offsetMin = new Vector2(10000,10000);
-        var offsetMax = new Vector2(-10000,-10000);
+        var width = EndRectItem.sizeDelta.x + ExtraWidthForBackground;
+        var height = math.abs(EndRectItem.localPosition.y) + EndRectItem.sizeDelta.y + ExtraHeightForBackground;
 
-        for (int i = 0; i < AllTextRectItems.Count; i++)
-        {
-            var rectItem = AllTextRectItems[i];
-            var scale = ScaleObject.localScale.x;
-            
-
-            var tempMin = rectItem.offsetMin * scale;
-            tempMin.y += rectItem.transform.localPosition.y; 
-            tempMin.x += rectItem.transform.localPosition.x; 
-
-            if (offsetMin.x > tempMin.x) offsetMin.x = tempMin.x;
-            if (offsetMin.y > tempMin.y) offsetMin.y = tempMin.y;
-
-            var tempMax = rectItem.offsetMax * scale;
-            tempMax.y += rectItem.transform.localPosition.y; 
-            tempMax.x += rectItem.transform.localPosition.x; 
-
-            if (offsetMax.x < tempMax.x) offsetMax.x = tempMax.x;
-            if (offsetMax.y < tempMax.y) offsetMax.y = tempMax.y; 
-
-            var textMeshPro = rectItem.GetComponent<TextMeshProUGUI>();
-            textMeshPro.ForceMeshUpdate();
-            rectItem.sizeDelta = new Vector2 (rectItem.sizeDelta.x, textMeshPro.GetRenderedValues(true).y);
-        }
-        Debug.Log(offsetMin + " " + offsetMax);
-        BackgroundRect.offsetMin = offsetMin;
-        BackgroundRect.offsetMax = offsetMax;
-    }
-
-    void СalculationDynamicRectSize()
-    {
-
-        for (int i = 0; i < AllTextRectItems.Count; i++)
-        {
-            var rectItem = AllTextRectItems[i];
-            var scale = rectItem.localScale.x;
-
-            var textMeshPro = rectItem.GetComponent<TextMeshProUGUI>();
-            textMeshPro.ForceMeshUpdate();
-
-            Vector2 textRect = textMeshPro.GetRenderedValues(true) * scale;
-
-            rectItem.sizeDelta = textMeshPro.GetRenderedValues(true);
-
-            BackgroundRect.sizeDelta = rectItem.sizeDelta;
-        }
-
-        
-
+        BackgroundRect.sizeDelta = new Vector2(width, height);
     }
 
 }
