2e62760c02ccf6891343a2c4f64ea370697b0256
Cor Lapus <kirisakisarim@gmail.com>
2024-09-30 02:39:16 +0300

    Исправление ошибок для билда

--- Assets/_Project/Scripts/NetworkSteam/InviteButton.cs
+++ Assets/_Project/Scripts/NetworkSteam/InviteButton.cs
@@ -1,15 +1,15 @@
+using Steamworks;
 using Steamworks.Data;
 using UnityEngine;
 
 
 public class InviteFriendButton : MonoBehaviour 
 {
-
     // Hooked up in Inspector 
     public void Invite()
     {
         if (Steamworks.SteamUtils.IsOverlayEnabled) { }
-        else { Debug.Log("Disabled!");}
+        else { Debug.Log("Disabled!"); }
 
         Lobby lobby = (Lobby)LobbySaver.instance.currentLobby;
         Steamworks.SteamFriends.OpenGameInviteOverlay(lobby.Id);
