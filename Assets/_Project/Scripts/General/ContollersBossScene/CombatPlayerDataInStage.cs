973ab6c2a7bf4d4275b1501f9edfdc97b2356180
Cor Lapus <kirisakisarim@gmail.com>
2024-09-30 04:24:39 +0300

    Продолжаем искать дебаг логами

--- Assets/_Project/Scripts/General/ContollersBossScene/CombatPlayerDataInStage.cs
+++ Assets/_Project/Scripts/General/ContollersBossScene/CombatPlayerDataInStage.cs
@@ -98,6 +98,7 @@ public class CombatPlayerDataInStage : MonoBehaviour
 
     void InitialiseArrayes()
     {
+        Debug.Log("InitialiseArrayes in CombatPlayerDataInStage");
         PlayersHeroes = new GameObject[playerCount];
         HeroCoordinates = new Vector2[playerCount];
     }
@@ -127,6 +128,8 @@ public class CombatPlayerDataInStage : MonoBehaviour
 
     public void UpdatePlayersHeroes(GameObject Hero, int PlayerId)
     {
+        Debug.Log(PlayersHeroes.Length);
+        Debug.Log("UpdatePlayersHeroes");
         PlayersHeroes[PlayerId] = Hero;
     }
 }
