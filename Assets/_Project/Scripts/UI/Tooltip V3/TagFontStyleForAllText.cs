e8a4b1c69ceeb895f070bae323ce53a0571ced4c
Gaida <kaliguri@bk.ru>
2024-10-10 17:46:03 +0300

    Minor Update

--- Assets/_Project/Scripts/UI/Tooltip V3/TagFontStyleForAllText.cs
+++ Assets/_Project/Scripts/UI/Tooltip V3/TagFontStyleForAllText.cs
@@ -5,21 +5,29 @@ using UnityEngine;
 public class TagFontStyleForAllText : MonoBehaviour
 {
     [Title("Style")]
-    public int StyleNumber = 0;
+    [SerializeField]public bool IsTooltip = true;
+    [DisableIf("IsTooltip")] [SerializeField] TagFontStyle Style;
+    [SerializeField] int FontStyleNumber = 0;
 
-    private TagFontStyle Style;
+    private TagFontStyle TooltipStyle;
     private TextMeshProUGUI textMeshPro => GetComponent<TextMeshProUGUI>();
     // Start is called before the first frame update
     void Start()
     {
-        Style = gameObject.GetComponentInParent<TooltipV3ParentManager>().Style;
-
-        DataTransfer();
+        if (IsTooltip)
+        {
+        TooltipStyle = gameObject.GetComponentInParent<TooltipV3ParentManager>().Style;
+        DataTransfer(TooltipStyle);
+        }
+        else
+        {
+        DataTransfer(Style);
+        }
     }
 
-    void DataTransfer()
+    void DataTransfer(TagFontStyle style)
     {
-        var choiceStyle = Style.fontStyles[StyleNumber];
+        var choiceStyle = style.fontStyles[FontStyleNumber];
 
         textMeshPro.color = choiceStyle.color;
 
